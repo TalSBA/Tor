44601fa66d12a19c9f0cc66af73bfc93b346844c
TalSBA <60568087+TalSBA@users.noreply.github.com>
2021-05-04 10:28:00 +0300

    signup page design

--- src/Pages/CreateCalendar.js
+++ src/Pages/CreateCalendar.js
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router";
 
 function CreateCalendar({ activeUser }) {
+  const [showCreateError, setShowCreateError] = useState(false);
+
   if (!activeUser) {
     return <Redirect to="/login" />;
   }
   return (
-    <div>
-      <h1>Create Calendar</h1>
-    </div>
+    <div className="p-create-calendar">
+    {/* <Card>
+      <Card.Header>
+        {" "}
+        <h1>ברוכים הבאים לתור!</h1>
+        <p>אנא מלא את הפרטים הבאים</p>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>
+          {showInvalidLogin ? (
+            <Alert variant="danger">אחד הפרטים לא תקינים!</Alert>
+          ) : null}
+          <Form onSubmit={signup}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>שם העסק</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="שם העסק"
+                value={bName}
+                onChange={(e) => setBName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>תחום</Form.Label>
+              <Form.Control as="select" value={type} onChange={(e) => setBType(e.target.value)}>
+                <option>מספרה</option>
+                <option>קוסמטיקה</option>
+                <option>אחר</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>טלפון</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="טלפון"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="success" type="submit" block>
+              צור יומן
+            </Button>
+          </Form>
+        </Card.Text>
+      </Card.Body>
+    </Card> */}
+  </div>
   );
 }
 
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Card, Form, Alert, Button } from "react-bootstrap";
+import "../Styles/Signup.css";
 
 function Signup(props) {
-    return (
-        <div>
-            <h1>Signup</h1>
-        </div>
-    );
+  const [showSignupError, setShowsignupError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [fullName, setName] = useState("");
+
+  function signup() {}
+  return (
+    <div className="p-signup">
+      <Card>
+        <Card.Header>
+          {" "}
+          <h1>ברוכים הבאים לתור!</h1>
+          <p>אנא מלא את הפרטים הבאים</p>
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>
+            {showSignupError ? (
+              <Alert variant="danger">אחד הפרטים לא תקינים!</Alert>
+            ) : null}
+            <Form onSubmit={signup}>
+              <Form.Group controlId="formBasicFullName">
+                <Form.Label>שם מלא</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="שם מלא"
+                  value={fullName}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>אימייל</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="הזן אימייל"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>סיסמא</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="סיסמא"
+                  value={pwd}
+                  onChange={(e) => setPwd(e.target.value)}
+                />
+              </Form.Group>
+
+              <Button variant="success" type="submit" block>
+                הרשם
+              </Button>
+            </Form>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Signup;
