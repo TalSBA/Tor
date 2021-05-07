1b0ccdbc4b2b7fc4e17ce1e35e267b4afcc69682
TalSBA <60568087+TalSBA@users.noreply.github.com>
2021-05-07 16:22:40 +0300

    add confirm page

--- src/Pages/ConfirmCalendar.js
+++ src/Pages/ConfirmCalendar.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Card } from "react-bootstrap";
+import "../Styles/ConfirmCalendar.css";
 
-function ConfirmCalendar(props) {
-    return (
-        <div>
-            {console.log(props)}
-        </div>
-    );
+function ConfirmCalendar({ calendar }) {
+  return (
+    <div className="p-confirm-calendar">
+      <Card>
+        <Card.Header>
+          <img width="350" height="200" src={calendar?.image} />
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>
+            <center>
+              <div
+                style={{
+                  paddingBottom: "19px",
+                  fontSize: "30px",
+                  color: "darkorange",
+                }}
+              >
+                {" "}
+                מזל טוב!{" "}
+              </div>
+              <div style={{ paddingBottom: "10px", fontSize: "24px" }}>
+                {calendar.name}{" "}
+              </div>
+              <div style={{ fontSize: "18px" }}>
+                יומן העסק שלך נוצר בהצלחה!
+                <br />
+                מעכשיו הלקוחות שלך יוכלו לקבוע תורים בעצמם דרך הלינק הבא:
+                <br />
+                <a
+                  href="http://localhost:3000/?#/scheduale-appointment/0"
+                  target="_blank"
+                >
+                  http://localhost:3000/?#/scheduale-appointment/0
+                </a>
+              </div>
+            </center>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default ConfirmCalendar;
